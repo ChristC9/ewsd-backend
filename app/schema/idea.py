dfb9dd8f574942db9e9e53625ec7372dd8ec90f1
thiha-onow <152261947+thiha-onow@users.noreply.github.com>
2025-03-02 11:13:06 +0700

    ✨ finish leaderboards

--- app/schema/idea.py
+++ app/schema/idea.py
@@ -15,7 +15,6 @@ class IdeasListRequest(PaginationRequest):
     filter_department: Optional[List[int]] = Field(None, alias="filter[department]")
 
         
-
 class IdeaResponse(BaseModel):
     id: int
     title: str
