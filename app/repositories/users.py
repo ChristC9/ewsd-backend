1060c0fa4d8e70655a860cc32b52a037c7f31f54
Chris <nyanhtetaungucsy106@gmail.com>
2025-04-30 14:42:32 +0700

    username no space fix

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -18,6 +18,8 @@ class UserRepository:
     def __init__(self,db: AsyncSession):
         self.db = db
 
+    def remove_spaces(self, string: str) -> str:
+        return string.replace(" ", "")
 
     async def get_user(self,username: str = None, user_id: int = None, email: str = None) -> User:
 
@@ -83,7 +85,7 @@ class UserRepository:
             db_user = User(
                 firstname=user.firstname,
                 lastname=user.lastname,
-                username=user.username,
+                username=self.remove_spaces(user.username),
                 default_pwd = default_hashed_password,
                 password=hashed_password,
                 email= user.email if user.email else None,
